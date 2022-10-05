2f5252e9915372b4c365f9cc070e63529b2bc2e0
Aleksandr Molostov <molostov.alex.nik@gmail.com>
2022-10-05 15:22:21 +0300

    первый, самый простой способ решения задачи

--- AreaFigureConsoleApp/Program.cs
+++ AreaFigureConsoleApp/Program.cs
@@ -6,7 +6,74 @@ namespace AreaFigureConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double radiusCircle = 5;
+            Console.WriteLine($"Круг с радиусом {radiusCircle}");
+            PrintArea(radiusCircle);
+            double a = 3;
+            double b = 4;
+            double c = 5;
+            Console.WriteLine($"Треугольник со сторонами {a}, {b}, {c}");
+            PrintArea(a, b, c);
+            Console.WriteLine($"Прямоугольник со сторонами {b}, {c}");
+            PrintArea(b,c);
+        }
+
+        private static void PrintArea(double a, double b, double c)
+        {
+            if (CheckExistenceTriangle(a, b, c))
+            {
+                Console.WriteLine(GetAreaTriangle(a, b, c));
+            }
+            else
+            {
+                Console.WriteLine("There is no such triangle");
+            }
+        }
+
+        private static void PrintArea(double a, double b)
+        {
+            Console.WriteLine(GetAreaRectangle(a, b));
+        }
+
+        private static void PrintArea(double radius)
+        {
+            Console.WriteLine(GetAreaCircle(radius));
+        }
+
+
+        static double GetAreaCircle(double radius)
+        {
+            return 2 * Math.PI * Math.Pow(radius, 2);
+        }
+        static double GetAreaRectangle(double a, double b)
+        {
+            return a*b;
+        }
+
+        static double GetAreaTriangle(double side1, double side2, double side3)
+        {
+            var p = side1 + side2 + side3;
+            var s = Math.Sqrt((p * (p - side1) * (p - side2) * (p - side3)));
+            return s;
+        }
+
+        private static bool CheckExistenceTriangle(double a, double b, double c)
+        {
+            var existence = true;
+
+            if (!((a + b) > c))
+            {
+                existence = false;
+            }
+            if (!((a + c) > b))
+            {
+                existence = false;
+            }
+            if (!((b + c) > a))
+            {
+                existence = false;
+            }
+            return existence;
         }
     }
 }
