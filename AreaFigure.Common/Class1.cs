e4d915727979ba1f9fc3e49c9bbe03d37df7dd0d
Aleksandr Molostov <molostov.alex.nik@gmail.com>
2022-10-05 11:29:05 +0300

    Add project files.

---
+++ AreaFigure.Common/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace AreaFigure.Common
+{
+    public class Class1
+    {
+    }
+}
---
+++ AreaFigureConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AreaFigureConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
