d30a2ce183379a331c4226197d8fcff0984fbb97
Aleksandr Molostov <molostov.alex.nik@gmail.com>
2022-10-05 22:42:05 +0300

    - поправил Юнит-Тест

--- AreaFigureUnitTest/FigureManager.cs
+++ AreaFigureUnitTest/FigureManager.cs
@@ -7,22 +7,23 @@ namespace AreaFigureUnitTest
 {
     public class FigureManager_TestClass
     {
+        FigureManager figureManager = new FigureManager();
 
         [Theory]
         [InlineData("1 1 1", 0.433012)]
         [InlineData("3 4 5", 6)]
         public void OutputSquareFigure_TriangleSquare_TrueResult(string input, double output)
         {
-            FigureManager figureManager = new FigureManager();
+            
             var values = figureManager.InputValues(input);
             var figure = figureManager.CreateFigure(values);
 
             var resultText = figureManager.OutputSquareFigure(figure);
             var resultDouble = Convert.ToDouble(resultText);
 
-            resultDouble = Math.Round(resultDouble, 5);
+            var result = Math.Round(resultDouble, 5);
             output = Math.Round(output, 5);
-            Assert.Equal(output, resultDouble);
+            Assert.Equal(output, result);
         }
         
     }
