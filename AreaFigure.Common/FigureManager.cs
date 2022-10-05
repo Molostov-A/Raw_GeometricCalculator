622c37f0673e165c58a265ddebff8816acea3595
Aleksandr Molostov <molostov.alex.nik@gmail.com>
2022-10-05 21:35:22 +0300

    Вывел все функции в статический класс FigureManager в библиотеке

--- AreaFigure.Common/FigureManager.cs
+++ AreaFigure.Common/FigureManager.cs
@@ -1,4 +1,7 @@
-﻿using AreaFigure.Common.Figures;
+﻿using AreaFigure.Common.Exceptions;
+using AreaFigure.Common.Figures;
+using System;
+using System.Linq;
 
 namespace AreaFigure.Common
 {
@@ -19,6 +22,62 @@ namespace AreaFigure.Common
                 return new Triangle(figure.LenghSides);
 
             return new Polygone(figure.LenghSides);
+        }
+
+        public static string[] TrySplitToArray(string str)
+        {
+            try
+            {
+                var value = str.Split();
+                return value;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+
+        public static Figure TryExistenceFigure(double[] values)
+        {
+            try
+            {
+                Figure.СheckExistenceFigure(values);
+                return new Figure(values);
+            }
+            catch (FigureDoesNotExist e)
+            {
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+
+        public static double[] InputValues(string str)
+        {
+            if (str == "")
+            {
+                return new double[0];
+            }
+
+            str = str.Trim();
+
+            var value = FigureManager.TrySplitToArray(str).Select(double.Parse).ToArray();
+
+            return Figure.CollapseNullValues(value);
+        }
+
+        public static string OutputSquareFigure(Figure figure)
+        {
+            string ansver;
+            try
+            {
+                ansver = figure.GetSquare().ToString();
+            }
+            catch (FeatureNotImplemented e)
+            {
+                ansver = e.Message;
+            }
+            return ansver;
         }
     }
 }
--- AreaFigureConsoleApp/Program.cs
+++ AreaFigureConsoleApp/Program.cs
@@ -17,7 +17,8 @@ namespace AreaFigureConsoleApp
             {
                 Console.WriteLine($"Type of figure - {figure.GetТypeFigureString()}");
                 CheckOnTriangleRectangular(figure);
-                PrintSquareFigure(figure);
+                Console.WriteLine($"Square = {FigureManager.OutputSquareFigure(figure)}");
+                
             }
         }
 
@@ -43,65 +44,8 @@ namespace AreaFigureConsoleApp
             Console.WriteLine("One number - length of the radius of the circle");
             Console.WriteLine("Two values - the length of the sides of the rectangle");
             Console.WriteLine("The three values - lengths of the sides of the triangle");
-            var values = InputValues();
-            return TryExistenceFigure(values);
-        }
-
-        private static void PrintSquareFigure(Figure figure)
-        {
-            string ansver;
-            try
-            {
-                ansver = figure.GetSquare().ToString();
-            }
-            catch (FeatureNotImplemented e)
-            {
-                ansver = e.Message;
-            }
-            Console.WriteLine($"Square = {ansver}");
-        }
-
-        private static double[] InputValues()
-        {
-            var str = Console.ReadLine();
-            if (str == "")
-            {
-                return new double[0];
-            }
-
-            str = str.Trim();
-
-            var value = TrySplitToArray(str).Select(double.Parse).ToArray();
-
-            return Figure.CollapseNullValues(value);
-        }
-
-        private static string[] TrySplitToArray(string str)
-        {
-            try
-            {
-                var value = str.Split();
-                return value;
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                throw;
-            }
-        }
-
-        private static Figure TryExistenceFigure(double[] values)
-        {
-            try
-            {
-                Figure.СheckExistenceFigure(values);
-                return new Figure(values);
-            }
-            catch (FigureDoesNotExist e)
-            {
-                Console.WriteLine(e.Message);
-                return null;
-            }
+            var values = FigureManager.InputValues(Console.ReadLine());
+            return FigureManager.TryExistenceFigure(values);
         }
     }
 }
