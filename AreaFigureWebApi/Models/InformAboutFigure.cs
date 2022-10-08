24e02007df7961d78f16ffea6760ea67877f6499
Aleksandr Molostov <molostov.alex.nik@gmail.com>
2022-10-08 13:40:46 +0300

    Добавил класс InformAboutFigure (для вывода информации)

---
+++ AreaFigureWebApi/Models/InformAboutFigure.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace AreaFigureWebApi.Models
+{
+    public class InformAboutFigure
+    {
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+
+        [JsonPropertyName("sides")]
+        public double[] Sides { get; set; }
+
+        [JsonPropertyName("square")]
+        public double Square { get; set; }
+    }
+}
