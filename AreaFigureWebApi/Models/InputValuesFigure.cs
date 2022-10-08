f5749fc8af34fb22653b55d359130232a3edeb02
Aleksandr Molostov <molostov.alex.nik@gmail.com>
2022-10-08 13:43:38 +0300

    добавил класс InputValuesFigure (для ввода информации)

---
+++ AreaFigureWebApi/Models/InputValuesFigure.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace AreaFigureWebApi.Models
+{
+    public class InputValuesFigure
+    {
+        [JsonPropertyName("values")]
+        public double[] Values { get; set; }
+    }
+}
