0f3be6d267a4c8bd41192d4164e2966025b195ee
Aleksandr Molostov <molostov.alex.nik@gmail.com>
2022-11-07 15:39:44 +0300

    Добавил общий класс для фигур

---
+++ AreaFigure.Common/Shapes/Shape.cs
@@ -0,0 +1,38 @@
+﻿using AreaFigure.Common.Abstract;
+using System.Linq;
+
+namespace AreaFigure.Common.Figures
+{
+    public class Shape : ISquareShape, IShape
+    {
+        public readonly double[] LenghSides;
+        public Shape(double[] lenghSides)
+        {
+            LenghSides = CollapseNullValues(lenghSides);
+        }
+
+        /// <summary>
+        /// Get the square of the figure
+        /// </summary>
+        /// <returns></returns>
+        public virtual double GetSquare()
+        {
+            return -1;
+        }
+        public virtual string GetTypeShape()
+        {
+            return "shape";
+        }
+
+        /// <summary>
+        /// If there are zeros among the lengths, they are removed from the list of sides
+        /// </summary>
+        /// <param name="sides"></param>
+        /// <returns> </returns>
+        private static double[] CollapseNullValues(double[] sides)
+        {
+            var notZeroSide = sides.Select(x => x).Where(x => x != 0).ToArray();
+            return notZeroSide;
+        }
+    }
+}
