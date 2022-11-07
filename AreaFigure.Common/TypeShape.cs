e1a8c829a3ddaccba0b06b92cd1621cffa85c3b1
Aleksandr Molostov <molostov.alex.nik@gmail.com>
2022-11-07 20:11:55 +0300

    Изменил структуру списка типов фигур

--- AreaFigure.Common/TypeShape.cs
+++ AreaFigure.Common/TypeShape.cs
@@ -1,13 +1,21 @@
-﻿using System.ComponentModel;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace AreaFigure.Common
 {
-    public enum TypeShape
+    public static class TypeShape
     {
-        point,
-        circle,
-        rectangle,
-        triangle,
-        polygon,
+        public static Dictionary<int, string> Roster = new Dictionary<int, string>()
+        {
+            {0, "point"},
+            {1, "circle"},
+            {2, "rectangle"},
+            {3, "triangle"}
+
+        };
+
+        public static string DefoultType = "polygone";
     }
+    
 }
