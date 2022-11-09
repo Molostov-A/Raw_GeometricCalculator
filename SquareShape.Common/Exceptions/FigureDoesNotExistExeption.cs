4797729f466b37b8e836fba859d23d0b8f2ce877
Aleksandr Molostov <molostov.alex.nik@gmail.com>
2022-11-09 20:56:32 +0300

    Оптимизировал кастомные ошибки

---
+++ SquareShape.Common/Exceptions/FigureDoesNotExistExeption.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace SquareShape.Common.Exceptions
+{
+    public class FigureDoesNotExistExeption : Exception
+    {
+        public FigureDoesNotExistExeption(string messege) : base(messege) { }
+    }
+}
