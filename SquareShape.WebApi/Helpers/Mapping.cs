084f29591f4ea6b647af6c3f4b7d2773a0065306
Aleksandr Molostov <molostov.alex.nik@gmail.com>
2022-11-10 09:39:00 +0300

    --

---
+++ SquareShape.WebApi/Helpers/Mapping.cs
@@ -0,0 +1,18 @@
+﻿using GeometricCalculator.Abstract;
+using SquareShape.WebApi.Models;
+
+namespace SquareShape.WebApi.Helpers
+{
+    public class Mapping
+    {
+        public static InformAboutFigure ToInformAboutFigure(IFigure figure)
+        {
+            InformAboutFigure infFigure = new InformAboutFigure()
+            {
+                Type = figure.Type,
+                Square = figure.Square
+            };
+            return infFigure;
+        }
+    }
+}
